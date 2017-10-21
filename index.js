fc5e291a7ce0d10586012dc343ff31ca2baa7320
AceMood <zmike86@163.com>
2017-10-22 00:46:10 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @file
+ */
+
+exports.setOptions = require('./lib/config').setOptions
+exports.getOptions = require('./lib/config').setOptions
+
+exports.middleware = require('./lib/middleware')
+exports.getUserInfo;
+exports.logout;
