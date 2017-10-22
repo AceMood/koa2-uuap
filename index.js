56c48c469142ca665ed5acaf85f6aecb067a3ec7
AceMood <zmike86@163.com>
2017-10-22 23:45:42 +0800

    dev

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 /**
- * @file
+ * @file export all api
  */
 
 exports.setOptions = require('./lib/config').setOptions
 exports.getOptions = require('./lib/config').setOptions
 
 exports.middleware = require('./lib/middleware')
-exports.getUserInfo;
-exports.logout;
+
+exports.getUserInfo = require('./lib/user').getUserInfo
+exports.setUserInfo = require('./lib/user').setUserInfo
+exports.resetUserInfo = require('./lib/user').resetUserInfo
+
+exports.logout = require('./lib/logout')
